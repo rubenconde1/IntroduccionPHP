c8cc08bd26d7310501cb7334046d3a42ea612f5a
rubenconde1 <rubencnd96@gmail.com>
2022-09-16 13:40:24 +0200

    Ejercicios hasta el 8

--- cadenas.php
+++ cadenas.php
@@ -1,3 +1,61 @@
+<!-- Funciones de cadena -->
+
+<!-- Ejercicio 1 -->
+<?php
+    $nombre = $_GET['nombre'] ?? 'Rubén';
+    $trimmed = trim($nombre, "/");
+    echo $trimmed
+?>
+
+<br>
+
+<!-- Ejercicio 2 -->
+<?php
+    echo strlen($nombre);
+?>
+
+<br>
+
+<!-- Ejercicio 3 -->
+<?php
+    echo strtoupper($nombre);
+?>
+
+<br>
+
+<!-- Ejercicio 4 -->
+<?php
+    echo strtolower($nombre);
+?>
+
+<br>
+
+<!-- Ejercicio 5 -->
+<?php
+    $prefijo = $_GET['prefijo'] ?? '';
+    echo $prefijo & $nombre
+?>
+
+<br>
+
+<!-- Ejercicio 6 -->
+<?php
+    echo substr_count(strtolower($nombre),'a');
+?>
+
+<br>
+
+<!-- Ejercicio 7 -->
 <?php
     
+    if (str_contains(strtolower($nombre), 'a')) {
+        echo stripos(strtolower($nombre),'a');
+    } else echo $nombre . " no contiene una a";
+?>
+
+<br>
+
+<!-- Ejercicio 8 -->
+<?php
+    echo str_ireplace('o',0,strtolower($nombre));
 ?>
