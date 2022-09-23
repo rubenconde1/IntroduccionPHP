3ca7296ea4e4f9cfac86fd03a43cabd5f6c90401
rubenconde1 <rubencnd96@gmail.com>
2022-09-23 13:49:34 +0200

    Ejercicio partlist en desarrollo

--- Funciones de arrays/partlist.php
+++ Funciones de arrays/partlist.php
@@ -1,5 +1,12 @@
 <!-- Partlist -->
 
 <?php
-    
+$array = ['Hola', 'mundo', 'bienvenido', 'a', 'tu', 'primer', 'partlist'];
+    function partlist($array){
+        for ($i=0; $i < count($array); $i++) { 
+            $separado = array_slice($array, 0, $i);
+        }
+    }
+
+    print_r(partlist($array));
 ?>
